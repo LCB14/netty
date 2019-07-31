22b0f685e02db8936e3bf9e544b45939aee2bbde
changbao.li <changbao.li@bianlifeng.com>
2019-07-31 11:15:21 +0800

    2019年07月31日11:15:20

--- common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
+++ common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
@@ -73,7 +73,7 @@ public abstract class MultithreadEventExecutorGroup extends AbstractEventExecuto
         }
 
         if (executor == null) {
-            // 创建线程选择器
+            // 创建线程执行器
             executor = new ThreadPerTaskExecutor(newDefaultThreadFactory());
         }
 
