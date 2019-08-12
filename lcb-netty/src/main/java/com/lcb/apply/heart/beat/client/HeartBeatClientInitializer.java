675e615679a2f8d014cafd2b26b56b899dc9ad5f
changbao.li <changbao.li@bianlifeng.com>
2019-08-12 16:55:54 +0800

    2019年08月12日16:55:53

---
+++ lcb-netty/src/main/java/com/lcb/apply/heart/beat/client/HeartBeatClientInitializer.java
@@ -0,0 +1,18 @@
+package com.lcb.apply.heart.beat.client;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class HeartBeatClientInitializer extends ChannelInitializer<Channel> {
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast(new HeartBeatClientHandler());
+    }
+}
