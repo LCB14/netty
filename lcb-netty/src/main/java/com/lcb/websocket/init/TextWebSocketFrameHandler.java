e681b8164f29dce5f9c6abb66b6d59ada88bef60
changbao.li <changbao.li@bianlifeng.com>
2019-08-23 23:01:26 +0800

    2019年08月23日23:01:25

---
+++ lcb-netty/src/main/java/com/lcb/websocket/init/TextWebSocketFrameHandler.java
@@ -0,0 +1,38 @@
+package com.lcb.websocket.init;
+
+import com.lcb.websocket.handler.HttpRequestHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        //当WebSocket连接成功
+        if (evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+            //不需要再处理HTTP请求
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            //告诉已经连接的客户端有新的客户端连接进来了
+            group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
+            //将新的客户端添加到ChannelGroup中
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        //消息转发给每一个客户端
+        group.writeAndFlush(msg.retain());
+    }
+}
