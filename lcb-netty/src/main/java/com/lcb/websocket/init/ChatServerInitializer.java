1c34fb9ab720a7dd69ec41029789325deaf96770
changbao.li <changbao.li@bianlifeng.com>
2019-08-24 01:52:23 +0800

    2019年08月24日01:52:23

--- lcb-netty/src/main/java/com/lcb/websocket/init/ChatServerInitializer.java
+++ lcb-netty/src/main/java/com/lcb/websocket/init/ChatServerInitializer.java
@@ -23,11 +23,36 @@ public class ChatServerInitializer extends ChannelInitializer<Channel> {
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
 
+        /**
+         * HttpServerCodec -- 将字节解码为HttpRequest、HttpContent和LastHttpContent.
+         * 并将HttpRequest、HttpContent和LastHttpContent编码为字节。
+         */
         pipeline.addLast(new HttpServerCodec());
+
+        /**
+         * 写入一个文件内容
+         */
         pipeline.addLast(new ChunkedWriteHandler());
+
+        /**
+         * 将一个HttpMessage和跟随它的多个HttpContent聚合为单个FullHttpRequest或者FullHttpResponse,
+         * 经过该handler处理之后数据流向下一个handler将会收到完整的HTTP请求或响应。
+         */
         pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+
+        /**
+         * 自定义处理FullHttpRequest（那些不发送到/ws URI的请求)
+         */
         pipeline.addLast(new HttpRequestHandler("/ws"));
+
+        /**
+         * 按照WebSocket规范要求，处理WebSocket升级握手、PingWebSocketFrame、PongWebSocketFrame、CloseWebSocketFrame
+         */
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        /**
+         * 处理TextWebSocketFrame和握手完成事件
+         */
         pipeline.addLast(new TextWebSocketFrameHandler(group));
     }
 }
