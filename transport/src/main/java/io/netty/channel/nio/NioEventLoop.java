67e88e5b5588142f36cf47188f166ae9b133a6c4
changbao.li <changbao.li@bianlifeng.com>
2019-09-04 22:50:19 +0800

    2019年09月04日22:50:18

--- transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
+++ transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
@@ -516,6 +516,9 @@ public final class NioEventLoop extends SingleThreadEventLoop {
                         /**
                          * 当一个客户端连接到服务器时，NIO底层会产生一个的OP_ACCEPT事件，然后NioEventLoop会通过processSelectedKey方法
                          * 调用到NioMessageUnsafe类的read方法，read方法随即会调用NioServerSocketChannel的doReadMessage方法.
+                         *
+                         * doReadMessage方法会构造一个NioSocketChannel，并添加到NioMessageUnsafe实例的成员变量readBuf集合中。
+                         * 随后，read方法会将这个集合中所有的Channel发送到管道中，由管道中的进站处理器（ChannelInboundHandler）进行处理。
                          */
                         processSelectedKeys();
 
