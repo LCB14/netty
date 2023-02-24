040aac57f6c2d62256fa6e6124c5fceb9a4f55c7
lichangbao <lichangbao@moqipobing.com>
2023-02-24 11:55:13 +0800

    2023年02月24日11:55:09

--- transport/src/main/java/io/netty/bootstrap/AbstractBootstrap.java
+++ transport/src/main/java/io/netty/bootstrap/AbstractBootstrap.java
@@ -368,8 +368,7 @@ public abstract class AbstractBootstrap<B extends AbstractBootstrap<B, C>, C ext
                     /**
                      * @see AbstractChannel#bind(SocketAddress, ChannelPromise)
                      */
-                    channel.bind(localAddress, promise)
-                            .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+                    channel.bind(localAddress, promise).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
                 } else {
                     promise.setFailure(regFuture.cause());
                 }
