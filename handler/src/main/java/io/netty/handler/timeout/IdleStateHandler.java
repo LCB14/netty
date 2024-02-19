78852688ea24a4cf048867326554c31eff1cb6e5
changbaol <1642272412@qq.com>
2024-02-19 21:38:27 +0800

    2024年02月19日21:38:23

--- handler/src/main/java/io/netty/handler/timeout/IdleStateHandler.java
+++ handler/src/main/java/io/netty/handler/timeout/IdleStateHandler.java
@@ -479,7 +479,7 @@ public class IdleStateHandler extends ChannelDuplexHandler {
             if (!reading) {
                 /**
                  * ticksInNanos() - lastReadTime -- 表示距离上一次读取数据已经过去多长时间了
-                 * nextDelay = nextDelay - （ticksInNanos() - lastReadTime）-- 表示距离下次检查是否有读操作还剩时间
+                 * nextDelay = nextDelay - （ticksInNanos() - lastReadTime）-- 表示距离下次检查是否有读操作还剩多长时间
                  */
                 nextDelay -= ticksInNanos() - lastReadTime;
             }
