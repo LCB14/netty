94e199ecbac40f28f05b2f642f6567db6dd57190
changbaol <1642272412@qq.com>
2025-03-26 18:08:12 +0800

    2025年03月26日18:08:07

--- transport/src/main/java/io/netty/channel/DefaultSelectStrategy.java
+++ transport/src/main/java/io/netty/channel/DefaultSelectStrategy.java
@@ -29,10 +29,12 @@ final class DefaultSelectStrategy implements SelectStrategy {
     @Override
     public int calculateStrategy(IntSupplier selectSupplier, boolean hasTasks) throws Exception {
         /**
+         * selectSupplier.get() 返回的是当前IO就绪的socket连接数。
+         *
          * 三种情况：
          * 1、存在异步执行任务且存在IO就绪事件  -- 返回一个大于 0 的值；
          * 2、存在异步执行任务但不存在IO就绪事件 -- 返回一个等于 0 的值；
-         * 3、无待执行的异步任务 -- 返回 -1；
+         * 3、无待执行的异步任务（等价于：无异步任务单即使有IO就绪事件也是返回 -1 😊） -- 返回 -1；
          */
         return hasTasks ? selectSupplier.get() : SelectStrategy.SELECT;
     }
